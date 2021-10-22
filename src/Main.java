7a720509c9de4b264f4be014ff439b0131a10d1a
Evgeny Zheleznov <zhel186@mail.ru>
2021-10-22 20:28:13 +0300

    deleted Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println("10 + 5 = " + calculator.add(10, 5));
-        System.out.println("10 + 15 = " + calculator.add(10, 15));
-    }
-}
